6b15ac2e1358e2be1c459b82de45af402da423fc
facon <cppapprentice@gmail.com>
2015-11-28 20:54:12 +0100

    Cambios escala.

---
+++ Unity/Assets/Scripts/BuildingPlacement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingPlacement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// FixedUpdate is called once per 0.33333 s
+	void FixedUpdate () {
+        Vector3 scale = this.transform.lossyScale;
+	}
+}
