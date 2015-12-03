d3f417e35a1c159b72b7354cc0214fa6215bc411
facon <cppapprentice@gmail.com>
2015-12-03 21:55:00 +0100

    Generación correcta de mapa

---
+++ Unity/Assets/Scripts/TileFeatures.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class TileFeatures : MonoBehaviour {
+    public int type;
+    public List<Material> materials = new List<Material>(5); 
+
+	// Use this for initialization
+	void Start () {
+        //MeshRenderer m = GetComponent<MeshRenderer>();
+        //m.material = new Material(Material.)
+	}
+
+    internal void init()
+    {
+        this.GetComponent<MeshRenderer>().material = materials[type];
+    }
+}
--- unity/Assets/Scripts/TileManager.cs
+++ unity/Assets/Scripts/TileManager.cs
@@ -1,12 +1,36 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System;
 
+//[ExecuteInEditMode]
 public class TileManager : MonoBehaviour {
     public GameObject tile;
-    public GameObject[,] tiles;
+    //public GameObject[,] tiles;
+    public List<List<GameObject>> tiles;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        int[,] map = parseMap();
+        int sizeX = (int)Mathf.Sqrt(map.Length);
+        int sizeZ = sizeX;
+
+        Debug.Log("Tamaño Array: " + map.Length);
+
+        for (uint z = 0; z < sizeZ; ++z)
+        {
+            for (uint x = 0; x < sizeX; ++x)
+            {
+                GameObject newTile = Instantiate(tile, new Vector3(x, 0, sizeZ - z), Quaternion.identity) as GameObject;
+                newTile.transform.parent = this.transform;
+                newTile.name = "Tile_x" + x + "_z" + z;
+                var features = newTile.GetComponent<TileFeatures>();
+                Debug.Log("X: " + x + ", Z: " + z);
+                features.type = map[z, x];
+                features.init();
+            }
+        }
+
+        /*
         var maxX = 20;
         var maxZ = 20;
 
@@ -21,10 +45,33 @@ public class TileManager : MonoBehaviour {
                 newTile.name = "Tile_x" + x + "_z" + z;
             }
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        */
+    }
+
+    private int[,] parseMap()
+    {
+        int sizeX = 10;
+        int sizeY = sizeX;
+
+        int[,] data = new int[10, 10]
+        {
+            { 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, },
+            { 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, },
+            { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, },
+            { 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, },
+            { 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, },
+            { 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, },
+            { 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, },
+            { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, },
+            { 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, },
+            { 1, 2, 2, 0, 0, 0, 0, 0, 0, 1, },
+        };
+
+        return data;
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
