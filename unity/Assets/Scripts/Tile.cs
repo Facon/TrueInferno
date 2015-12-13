c05fd29e6b98c76c4cc3b1a3d95bcca81c49e761
raulsg3 <raul.s3@hotmail.com>
2015-12-13 18:14:07 +0100

    Create Caesar-like UI frame and Resource Manager

--- unity/Assets/Scripts/Tile.cs
+++ unity/Assets/Scripts/Tile.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System;
 
 public class Tile : MonoBehaviour {
-    public uint posX, posZ;
+    public uint posX;
+    public uint posZ;
+
     public int type;
-    public List<Material> materials = new List<Material>(5);
+    public List<Material> materials;
 
 	// Use this for initialization
 	void Start () {
-        //MeshRenderer m = GetComponent<MeshRenderer>();
-        //m.material = new Material(Material.)
-	}
+    }
 
     internal void init()
     {
