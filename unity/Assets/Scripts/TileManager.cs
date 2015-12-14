b859b11e71e73b8c89513db8d75c965b54842d79
Alvraro <chachimente@yahoo.es>
2015-12-15 00:50:22 +0100

    Unify coords

--- unity/Assets/Scripts/TileManager.cs
+++ unity/Assets/Scripts/TileManager.cs
@@ -15,16 +15,9 @@ public class TileManager : MonoBehaviour
     public Tile tile;
 
     /// <summary>
-    ///  Matrix[z,x] with the tile GameObject. 
+    ///  Matrix[x,z] with the tile GameObject. 
     /// </summary>
     public Tile[,] tiles;
-
-    /// <summary>
-    ///  Matrix[z,x] with the tile type info. 
-    /// TODO A tile type should be attached somehow to its tile GameObject. It may be included
-    /// in a Component so its value could be changed by reading a specific message
-    /// </summary>
-    //public TileType[,] tileTypes;
 
     /// <summary>
     /// Char delim for tile names
@@ -35,112 +28,100 @@ public class TileManager : MonoBehaviour
     void Start()
     {
         int[,] map = parseMap();
-        //int sizeX = (int)Mathf.Sqrt(map.Length);
-        //int sizeZ = sizeX;
 
         Debug.Log("Tamaño Array: " + map.Length);
 
         tiles = new Tile[SIZE_X, SIZE_Z];
-        //tileTypes = new TileType[sizeZ, sizeX];
 
-        for (uint z = 0; z < SIZE_Z; ++z)
+        for (uint x = 0; x < SIZE_X; ++x)
         {
-            for (uint x = 0; x < SIZE_X; ++x)
+            for (uint z = 0; z < SIZE_Z; ++z)
             {
-                Tile newTile = Instantiate(tile, new Vector3(x, 0, SIZE_Z - z), Quaternion.identity) as Tile;
+                Tile newTile = Instantiate(tile, new Vector3(x, 0, z), Quaternion.identity) as Tile;
                 newTile.transform.parent = this.transform;
-                //newTile.name = "Tile_x" + x + "_z" + z;
-                newTile.name = getTileNameFromCoords(z, x);
+                newTile.name = getTileNameFromCoords(x, z);
                 newTile.tag = "Tile";
                 var features = newTile.GetComponent<Tile>();
                 //Debug.Log("X: " + x + ", Z: " + z);
                 features.posX = x;
                 features.posZ = z;
                 features.buildingType = TileType.EMPTY;
-                features.type = map[z, x];
+                features.type = map[x, z];
                 //Debug.Log("tile "+z+" "+x+" es de tipo "+features.type);
                 features.init();
 
                 // Update tile map
                 tiles[x, z] = newTile;
-
-                // TODO quick-fix...
-                // Update tile type map
-                //switch(map[z, x])
-                //{
-                  //  case 0: tileTypes[z, x] = TileType.EMPTY; break;
-                    //default: tileTypes[z, x] = TileType.OBSTACLE; break;
-                //}
             }
         }
     }
 
     /// <summary>
-    ///  Bean for our 2D points with z and x coordinates
+    ///  Bean for our 2D points with x and z coordinates
     /// </summary>
-    public class Vector2zx
+    public class Vector2xz
     {
-        public float z;
         public float x;
+        public float z;
 
-        public Vector2zx(float z, float x)
+        public Vector2xz(float x, float z)
         {
-            this.z = z;
             this.x = x;
+            this.z = z;
         }
     }
 
-    public Vector2zx getTileCoords(Tile tile)
+    public Vector2xz getTileCoords(Tile tile)
     {
         return getCoordsFromTileName(tile.name);
     }
 
-    public Vector2zx getCoordsFromTileName(String name)
+    public Vector2xz getCoordsFromTileName(String name)
     {
         String[] parts = name.Split(TILE_NAME_DELIM);
-        return new Vector2zx(float.Parse(parts[2]), float.Parse(parts[1]));
+        return new Vector2xz(float.Parse(parts[1]), float.Parse(parts[2]));
     }
 
-    public string getTileNameFromCoords(uint z, uint x)
+    public string getTileNameFromCoords(uint x, uint z)
     {
         return "Tile" + TILE_NAME_DELIM + x + TILE_NAME_DELIM + z;
     }
 
     /// <summary>
-    /// Convenience wrapper method for getCrossAdyacents(int z, int x)
+    /// Convenience wrapper method for getCrossAdyacents(int x, int z)
     /// </summary>
     /// <param name="v"></param>
     /// <returns></returns>
-    public HashSet<Vector2zx> getCrossAdyacents(Vector2zx v)
+    public HashSet<Vector2xz> getCrossAdyacents(Vector2xz v)
     {
-        return getCrossAdyacents((uint)v.z, (uint)v.x);
+        return getCrossAdyacents((uint)v.x, (uint)v.z);
     }
 
     /// <summary>
-    /// Get 4 cross-adyacent z,x coordinate pairs for a given pair of coordinates. e.g. for (1,2) returns {(1,1),(1,3),(0,2),(2,2)}.
+    /// Get 4 cross-adyacent x,z coordinate pairs for a given pair of coordinates. e.g. for (1,2) returns {(1,1),(1,3),(0,2),(2,2)}.
     /// Takes into consideration min and max values for each dimension
     /// </summary>
-    /// <param name="z"></param>
     /// <param name="x"></param>
+    /// <param name="z"></param>
     /// <returns></returns>
-    public HashSet<Vector2zx> getCrossAdyacents(uint z, uint x)
+    public HashSet<Vector2xz> getCrossAdyacents(uint x, uint z)
     {
-        if (z < 0 || z >= SIZE_Z || x < 0 || x >= SIZE_X)
-            throw new Exception("z=" + z + " must be in [0," + (SIZE_Z - 1) + "] and x=" + x + " must be in [0," + (SIZE_X - 1) + "]");
+        if (x < 0 || x >= SIZE_X || z < 0 || z >= SIZE_Z)
+            throw new Exception("x=" + x + " must be in [0," + (SIZE_X - 1) + "] and z=" + z + " must be in [0," + (SIZE_Z - 1) + "]");
 
-        HashSet<Vector2zx> ret = new HashSet<Vector2zx>();
-
-        if (z >= 1)
-            ret.Add(new Vector2zx(z - 1, x));
-
-        if (z <= SIZE_Z - 2)
-            ret.Add(new Vector2zx(z + 1, x));
+        HashSet<Vector2xz> ret = new HashSet<Vector2xz>();
 
         if (x >= 1)
-            ret.Add(new Vector2zx(z, x - 1));
+            ret.Add(new Vector2xz(x - 1, z));
 
         if (x <= SIZE_X - 2)
-            ret.Add(new Vector2zx(z, x + 1));
+            ret.Add(new Vector2xz(x + 1, z));
+
+        if (z >= 1)
+            ret.Add(new Vector2xz(x, z - 1));
+
+        if (z <= SIZE_Z - 2)
+            ret.Add(new Vector2xz(x, z + 1));
 
         return ret;
     }
