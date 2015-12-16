fee1976e63a076bf5016390038196bd118ade052
raulsg3 <raul.s3@hotmail.com>
2015-12-16 17:23:53 +0100

    Hito 1

--- unity/Assets/Scripts/ResourceManager.cs
+++ unity/Assets/Scripts/ResourceManager.cs
@@ -12,8 +12,8 @@ public class ResourceManager : MonoBehaviour {
     /// This velocities will be affected for some other factors (usually
     /// incremented) as the game progresses.
     /// </summary>
-    private const float RES_1_BASE_GATHERING_SPEED = 10.0f / 60.0f;
-    private const float RES_2_BASE_GATHERING_SPEED = 6.0f  / 60.0f;
+    private const float RES_1_BASE_GATHERING_SPEED = 20.0f / 60.0f;
+    private const float RES_2_BASE_GATHERING_SPEED = 12.0f  / 60.0f;
 
     /// <summary>
     /// Current gathering speed per minute and worker for both map resources,
