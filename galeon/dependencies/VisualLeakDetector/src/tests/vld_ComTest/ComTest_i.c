41d25a389a4be027bb628d1f576aa55a4886cda7
raulsg3 <raul.s3@hotmail.com>
2016-01-09 03:07:30 +0100

    Galeon: Including required exes and libraries

--- galeon/dependencies/VisualLeakDetector/src/tests/vld_ComTest/ComTest_i.c
+++ galeon/dependencies/VisualLeakDetector/src/tests/vld_ComTest/ComTest_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Mon Jan 04 19:57:47 2016
+/* at Wed Jan 06 01:17:05 2016
  */
 /* Compiler settings for ComTest.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
