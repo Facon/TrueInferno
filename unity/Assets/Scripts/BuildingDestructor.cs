a4e8fe49e76f34039d65f96e74e4ac782fd14548
Ruyman <kalgalath9@gmail.com>
2016-01-18 00:18:15 +0100

    agregado sistema de destruccion de edificios y un controlador de eventos

---
+++ unity/Assets/Scripts/BuildingDestructor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingDestructor : MonoBehaviour {
+
+	// Use this for initialization
+    public void DestroyBuilding(float delay) 
+    {
+        // Set Especial color for destruction
+        gameObject.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.blue);
+
+        // Removes Building from building list
+        TileManager tm = GameObject.Find("Map").GetComponent<TileManager>();
+        tm.buildingList.Remove(gameObject);
+
+        // Destroy the building in a delayed time
+        Destroy(gameObject, delay);
+    }
+}
---
+++ unity/Assets/Scripts/EventManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+    private TimeManager timeManager;
+    private TileManager tileManager;
+
+    //Event parameters
+    public float destroyInterval;
+    public float destroyDelay;
+    public int destroyChance;
+
+    // Event time controller
+    private float remainingDestroyEventTime;
+
+    private float previousTime;
+
+	// Use this for initialization
+	void Start () 
+    {
+        timeManager = GameObject.Find("TimeManager").GetComponent<TimeManager>();
+        tileManager = GameObject.Find("Map").GetComponent<TileManager>();
+        previousTime = 60 * timeManager.minutesTimeLimit;
+        remainingDestroyEventTime = destroyInterval;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Update event timers
+        remainingDestroyEventTime -= (previousTime - timeManager.remainingTime);
+
+        // Check for event timers
+        EventChecker();
+
+        // Update timer
+        previousTime = timeManager.remainingTime;
+	}
+
+
+    // Method for event controlling
+    void EventChecker() 
+    {
+        if (remainingDestroyEventTime <= 0)
+            BuildingDestructionEvent();
+    }
+
+    //Event Methods
+    void BuildingDestructionEvent()
+    {
+        remainingDestroyEventTime = destroyInterval;
+        bool eventSuccess = UnityEngine.Random.Range(1,100) <= destroyChance;
+        if ( eventSuccess && tileManager.buildingList.Count > 0)
+        {
+            GameObject targetBuilding = tileManager.buildingList[UnityEngine.Random.Range(0, tileManager.buildingList.Count)];
+            targetBuilding.GetComponent<BuildingDestructor>().DestroyBuilding(destroyDelay);
+        }
+    }
+}
