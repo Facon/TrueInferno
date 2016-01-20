8299152e9e9afce1615f0e2f61bcd8aee072596d
Ruyman <kalgalath9@gmail.com>
2016-01-20 23:43:04 +0100

    popup manager

---
+++ unity/Assets/Scripts/PopUpManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PopUpManager : MonoBehaviour {
+
+    public GameObject panel;
+
+    public void GeneratePopUp (float tamX, float tamY, string text) {
+        Transform canvastrans = GameObject.Find("Canvas").gameObject.transform;
+        GameObject popup = Instantiate(panel, new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
+        popup.transform.SetParent(canvastrans, false);
+        popup.transform.localScale = new Vector3(tamX, tamY, 0);
+        popup.GetComponentInChildren<Text>().text=text;
+    }
+
+}
