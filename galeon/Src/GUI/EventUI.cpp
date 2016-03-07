9949d1988b83ba62028f0c761582a3c1f9529ebf
Ruyman <kalgalath9@gmail.com>
2016-03-07 23:46:49 +0100

    g#provisionalui

--- galeon/Src/GUI/EventUI.cpp
+++ galeon/Src/GUI/EventUI.cpp
@@ -34,6 +34,9 @@ namespace GUI
 		// Activamos la interfaz de usuario
 		CEGUI::System::getSingletonPtr()->getDefaultGUIContext().getRootWindow()->addChild(_uiEventWindow);
 		setEventImage("EventTutorial1");
+		setEventTitle("Welcome to HELL !!!");
+		setEventText("maldito CEGUI debe morir esto es un puto asco lololol assier arreglame el master");
+		setEventTextResume("ASSIERTION HAS FAILED");
 		_uiEventWindow->setVisible(true);
 		_uiEventWindow->activate();
 	}
@@ -61,10 +64,20 @@ namespace GUI
 	{
 		_uiEventWindow->getChild("EventImage")->setProperty("Image","TrueInfernoEvents/" + eventImage);
 	}
+	
 	void EventUI::setEventText(std::string eventText)
 	{
 		_uiEventWindow->getChild("EventText")->setText(eventText);
 	}
 
+	void EventUI::setEventTitle(std::string eventTitle)
+	{
+		_uiEventWindow->getChild("EventTitle")->setText(eventTitle);
+	}
+
+	void EventUI::setEventTextResume(std::string EventTextResume)
+	{
+		_uiEventWindow->getChild("EventTextResume")->setText(EventTextResume);
+	}
 	
 }
