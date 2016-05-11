d5d8375b093aec0815ca969a0a4a96ef5c66ac97
Ruyman <kalgalath9@gmail.com>
2016-05-11 23:34:33 +0200

    g#soulstrial

--- galeon/Src/GUI/BuildingSelectionUI.cpp
+++ galeon/Src/GUI/BuildingSelectionUI.cpp
@@ -125,7 +125,12 @@ namespace GUI
 		
 		
 		if (name == "Souls Trial"){
-			CEGUI::Editbox* editbox = (CEGUI::Editbox*)_uipopupWindow->getChild("HeavySoulsBurn");
+			//CEGUI::Editbox* editbox = (CEGUI::Editbox*)_uipopupWindow->getChild("HeavySoulsBurn");
+			unsigned int* getAvailableSouls = Logic::CSoulsTrialManager::getSingletonPtr()->getAvailableSouls();
+			_uipopupWindow->getChild("HeavySoulTotal")->setText(std::to_string(getAvailableSouls[Logic::CSoulsTrialManager::HEAVY]));
+			_uipopupWindow->getChild("WastedSoulTotal")->setText(std::to_string(getAvailableSouls[Logic::CSoulsTrialManager::WASTED]));
+			_uipopupWindow->getChild("LightSoulTotal")->setText(std::to_string(getAvailableSouls[Logic::CSoulsTrialManager::LIGHT]));
+			_uipopupWindow->getChild("UnknownSoulTotal")->setText(std::to_string(getAvailableSouls[Logic::CSoulsTrialManager::UNKNOWN]));
 
 		}
 		else
@@ -267,21 +272,21 @@ namespace GUI
 
 	bool BuildingSelectionUI::judgeSoulsReleased(const CEGUI::EventArgs& e)
 	{
-		int HeavyBurned = std::atoi(_uipopupWindow->getChild("HeavySoulsBurn")->getText().c_str());
-		int HeavyWorker = std::atoi(_uipopupWindow->getChild("HeavySoulsWork")->getText().c_str());
-		int WastedBurned = std::atoi(_uipopupWindow->getChild("WastedSoulsBurn")->getText().c_str());
-		int WastedWorker = std::atoi(_uipopupWindow->getChild("WastedSoulsWork")->getText().c_str());
-		int LightBurned = std::atoi(_uipopupWindow->getChild("LightSoulsBurn")->getText().c_str());
-		int LightWorker = std::atoi(_uipopupWindow->getChild("LightSoulsWork")->getText().c_str());
-		int LukuaunBurned = std::atoi(_uipopupWindow->getChild("LukuaunSoulsBurn")->getText().c_str());
-		int LukuaunWorker = std::atoi(_uipopupWindow->getChild("LukuaunSoulsWork")->getText().c_str());
+		unsigned int soulstowork[4];
+		unsigned int soulstoburn[4];
 
-		printf("heavy sousl burned %i working %i\n", HeavyBurned, HeavyWorker);
-		printf("wasted sousl burned %i working %i\n", WastedBurned, WastedWorker);
-		printf("light sousl burned %i working %i\n", LightBurned, LightWorker);
-		printf("lukuaun sousl burned %i working %i\n", LukuaunBurned, LukuaunWorker);
+		soulstowork[Logic::CSoulsTrialManager::HEAVY] = std::atoi(_uipopupWindow->getChild("HeavySoulsWork")->getText().c_str());
+		soulstowork[Logic::CSoulsTrialManager::WASTED] = std::atoi(_uipopupWindow->getChild("WastedSoulsWork")->getText().c_str());
+		soulstowork[Logic::CSoulsTrialManager::LIGHT] = std::atoi(_uipopupWindow->getChild("LightSoulsWork")->getText().c_str());
+		soulstowork[Logic::CSoulsTrialManager::UNKNOWN] = std::atoi(_uipopupWindow->getChild("UnknownSoulsWork")->getText().c_str());
 
-		closeWindow();
+		soulstoburn[Logic::CSoulsTrialManager::HEAVY] = std::atoi(_uipopupWindow->getChild("HeavySoulsBurn")->getText().c_str());
+		soulstoburn[Logic::CSoulsTrialManager::WASTED] = std::atoi(_uipopupWindow->getChild("WastedSoulsBurn")->getText().c_str());
+		soulstoburn[Logic::CSoulsTrialManager::LIGHT] = std::atoi(_uipopupWindow->getChild("LightSoulsBurn")->getText().c_str());
+		soulstoburn[Logic::CSoulsTrialManager::UNKNOWN] = std::atoi(_uipopupWindow->getChild("UnknownSoulsBurn")->getText().c_str());
+
+			if (Logic::CSoulsTrialManager::getSingletonPtr()->createSouls(soulstowork, soulstoburn) == Logic::CSoulsTrialManager::NONE)
+				closeWindow();
 
 		return true;
 	}
